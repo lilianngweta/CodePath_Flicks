84ef97de18fb9b3a8607fd4aaf077541496772a4
lilianngweta <lilian.ngweta@asu.edu>
2016-01-25 18:55:55 -0700

    Initial Commit

---
+++ CodePath_Flicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodePath_Flicks
+//
+//  Created by Lilian Ngweta on 1/25/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
